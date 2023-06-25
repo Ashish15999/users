e029f7bf0313af6af5d2d62f325a7df588cba4af
Ashish Sharma <ashish15999@gmail.com>
2023-06-25 18:32:26 +0530

    second commit

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bcrypt = require("bcryptjs")
 const mongodb = require("mongodb")
 
 
@@ -13,27 +14,40 @@ const db = require("./data/database")
 app.use(express.urlencoded({extended:false}))
 
 app.post("/register/users", async (req, res) => {
+    const enterPassword = req.body.password
+    const encodedPass = await bcrypt.hash(enterPassword , 12)
     const userdata = 
     { 
         username : req.body.username,
         email: req.body.email,
-        password : req.body.password
+        password : encodedPass
     }
-    const result = await db.getdb().collection("users").insertOne(userdata)
-    console.log(result)
+    await db.getdb().collection("users").insertOne(userdata)
     res.redirect("/login");
 });
 
 app.post("/login/users", async (req, res) => {
-    const users  = await db.getdb().collection("users").find().toArray()
-    for (let i of users){
-        if ((i.username === req.body.username) && (i.password === req.body.password)) {
-            res.redirect("/welcome")
-        }
-        else{
-            res.redirect("/login")
-        }
-}});
+    const enterusername = req.body.username
+    const enterPassword = req.body.password
+
+    const userExist = await db.getdb().collection("users").findOne({username : enterusername})
+
+    if (!userExist){
+        console.log("user not exist")
+        return res.redirect("/login")
+    }
+
+    const comparepass = await bcrypt.compare(enterPassword , userExist.password)
+
+    if(!comparepass){
+        console.log("enter Password Correctly")
+        return res.redirect("/login")
+    }
+
+    console.log("Have welcome page access")
+    res.redirect("/welcome")
+        
+});
 
 
 app.post("/welcome/notes", async (req, res) => {
